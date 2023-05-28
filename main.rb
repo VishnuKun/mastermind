fa1da8d802956a8cdb9e722c33e92b073c574c52
Vishnu <vishnukun954AV@protonmail.com>
2023-05-28 19:55:04 +0530

    Added code generator method for the computer.

--- main.rb
+++ main.rb
@@ -3,12 +3,29 @@
 class MasterMind
   def guess; end
   # initial colors => 'red', 'green', 'yellow', 'blue', 'magenta', 'black'
+  @@color_array = %w[red green yellow blue magenta black]
+
   # codes => random 4 from colors
-  # '⭕' => correct color at wrong position
-  # '❌' => correct color at correct position 
+  # '⭕' => correct color at correct position
+  # '❌' => correct color at wrong position
   # end game on turn exceeding maximum 12
   # ask user to be creator of code or guesser
   # get random color code
+  def code_generator
+    @code = []
+    # push 4 random colors into the code
+    # from the colors array
+    4.times do
+      # push 1 random color into the code
+      # remove it from the color array as well
+      @code << @@color_array.delete_at(rand(@@color_array.size - 1))
+    end
+    @code
+  end
+
   # get guess from Human/Computer
   # return feedback message
 end
+
+x = MasterMind.new
+x.code_generator
