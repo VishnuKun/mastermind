06ba486e7d6eb27399f96c32ea6d1afd82a24c98
Vishnu <vishnukun954AV@protonmail.com>
2023-11-10 18:34:47 +0530

    Removed comments

--- main.rb
+++ main.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
 require_relative './module'
+# MasterMind
 class MasterMind
   include MyMethods
 
   # initial colors => 'red', 'green', 'yellow', 'blue', 'magenta', 'black'
   @@color_array = %w[red green yellow blue magenta black]
-  # store code
   @@code = []
 
-  # mix in
   # return feedback message
   def feedback_message(guess)
     @guess = guess
@@ -17,50 +16,36 @@ class MasterMind
     # '🟢' => correct color at correct position
     # '⭕' => correct color at wrong position
     # '⚫' => color not used in code
-    # store matches
     @matched = []
-    # iterate through the array and check for matches
-    # if an element matches then check for indexes as well
     @guess.each do |color|
-      # push element that matches with any code element
       @matched << if @@code.include?(color)
                     color
                   else
-                    # if not, then push '_' to the feedback
                     '_'
                   end
     end
-    # now check if matches were
     4.times do |i|
       @feedback << if @matched[i - 1] == @@code[i - 1]
-                     # if element matches with code's element
                      '🟢'
                    elsif @matched[i - 1] == '_'
-                     # if element matches with '_'
                      '⚫'
                    else
                      '⭕'
                    end
     end
-    # return the feedback
     @feedback.shuffle
   end
 
   # initialize
   def initialize
     @player = ask
-    # check if player is master mind or codebreaker
     if @player == 'mastermind'
       @@code = set_code
       get_guess('Computer')
       @guess = give_code
     else
-      # generate the random code
-      # set the code
       @@code = code_generator(@@color_array)
-      # guess the code
       @guess = get_guess('Human')
-      # display feedback
     end
     feedback_message(@guess)
     game_over
--- module.rb
+++ module.rb
@@ -1,15 +1,14 @@
 # frozen_string_literal: true
 
+# contains methods for mastermind
 module MyMethods
   # ask the mastermind to set the code for game
   def set_code
     @code = nil
     loop do
-      # get the code
       puts "\r"
       print 'Set the code for game, MasterMind : '
       @input = gets.chomp
-      # check if the code given is valid or not
       if @input.split.length == 4
         @code = @input
         break
@@ -24,19 +23,13 @@ module MyMethods
   # end game on turn exceeding maximum 12
   # ask user to be creator of code or guesser
   def ask
-    # flag for ending loop
     @flag = false
-    # store user choice
     @choice = nil
-    # keep asking until user gives correct answer
     while @flag == false
-      # display statements
       puts "\r"
       puts 'Would you like to play as Master Mind or Code Breaker?'
       print 'Type MM to play as Master Mind or type CB to play as Code Breaker : '
-      # get user input
       @input = gets.chomp
-      # display chosen choice
       puts "\r"
       if @input.downcase == 'mm'
         puts "You've chosen mastermind "
@@ -52,7 +45,6 @@ module MyMethods
         @flag = false
       end
     end
-    # return player choice
     @choice
   end
 
@@ -60,11 +52,7 @@ module MyMethods
   def code_generator(color_array)
     @color_array = color_array
     @code = []
-    # push 4 random colors into the code
-    # from the colors array
     4.times do
-      # push 1 random color into the code
-      # remove it from the color array as well
       @code << @color_array.delete_at(rand(@color_array.size - 1))
     end
     @code
@@ -81,11 +69,8 @@ module MyMethods
     @turns = 0
     puts 'Make sure the spellings are correct!'
     puts "Available colors are 'red', 'green', 'yellow', 'blue', 'magenta' and 'black'."
-    # run loop til all matches are '🟢' or turns are finished
     loop do
-      # get guess from Player
       print "Enter your guess #{user} : "
-      # check user
       if user == 'Computer'
         @guess = give_code
         print @guess.join(' ')
@@ -102,22 +87,17 @@ module MyMethods
         puts "\r"
         puts 'Please enter 4 colors only! Please try again.'
       else
-        # call feedback method
         @answer = feedback_message(@guess)
-        # check if all answers are '🟢' or not
         print 'Feedback Message : '
         puts @answer.join
         if @answer.all? { |answer| answer == '🟢' }
           puts 'Correct guess! You won!'
           break
         else
-          # show user feedback
           puts 'Wrong guess! Keep trying.'
         end
       end
-      # input should only be 4 characters long
       @turns += 1
-      # break if turns reach max 12
       if @turns == 12
         puts 'Your turns are over! You lost the game. Better luck next time.'
         break
